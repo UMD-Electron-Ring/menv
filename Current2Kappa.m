0d649e03e92ca50df0c95ac6a7ce86bc99b10bfd
kruisard <kierstenjean@gmail.com>
2018-04-06 23:33:11 +0100

    Fixed error regarding dipole focusing gradients

--- Current2Kappa.m
+++ Current2Kappa.m
@@ -11,6 +11,7 @@ E = 10.0e3;
 T = q*E;
 gamma = 1+T/(M*c^2);
 beta = sqrt(1-gamma^-2);
+ridg = (gamma*M*beta*c/q); % beam rigidity
 
 % MODEL PARAMETERS -------------------------------------------------------
 % -- quad model
@@ -24,32 +25,35 @@ B_off = 1.35; % offset [Gauss]
 B_sbfact = sqrt(0.6945); % SB HE factor
 
 % -- dipole model
-gint_dipox = 0.146e-4; % int. quad grad By in dipo (due to sext.component) [T/A]
-gint_dipoy = 0.565e-4; % int. quad grad Bx in dipo (due to edge-focusing component) [T/A]
+leff_dipo = 3.819e-2; % HE dipole length [m]
+g0_dipox = 0.146e-4/leff_dipo; % quad grad By in dipo (due to sext.component) [T/A]
+g0_dipoy = 0.565e-4/leff_dipo; % quad grad Bx in dipo (due to edge-focusing component) [T/A]
 d_sbfact  = 1; % Has not yet been calculated for dipoles.
+
 
 % -- YQ/QR1 model
 g0_panofsky = 1.01e-2; % quad peak grad per amp [T/m/A]
 panofsky_sbfact = 0.7224; 
 
 % -- PD model
-gint_pdx = 0.; % int. quad grad in PD (due to sext.component) has not been calc.
-gint_pdy = 0.041e-4; % int. quad grad in PD (due to sext.component) [T/A]
+leff_pd  = 5.006e-2; %HE PD length [m] 
+g0_pdx = 0./leff_pd; % HE quad grad in PD (due to sext.component) has not been calc.
+g0_pdy = 0.041e-4/leff_pd; % HE quad grad in PD (due to sext.component) [T/A]
 pd_sbfact  = 1; % Has not yet been calculated for dipoles.
 % ------------------------------------------------------------------------
 
 for i=1:N
 
 if( nargin==1 || (nargin==2 && ele(i)=='Q') )
-   kappa(i) = (I(i)*g0)/(gamma*M*beta*c/q)*q_sbfact;
+   kappa(i) = (I(i)*g0)/ridg*q_sbfact;
 elseif( nargin==2 && ele(i)=='S' )
-	kappa(i) = ((I(i)*B0+B_off)*B_sbfact/(1e4*2*(gamma*M*beta*c/q)))^2;
+	kappa(i) = ((I(i)*B0+B_off)*B_sbfact/(1e4*2*ridg))^2;
 elseif( nargin==2 && ele(i)=='Y' )
-    kappa(i) = (I(i)*g0_panofsky)/(gamma*M*beta*c/q)*panofsky_sbfact;
+    kappa(i) = (I(i)*g0_panofsky)/ridg*panofsky_sbfact;
 elseif( nargin==2 && ele(i)=='D' )
-    kappa(i) = (I(i)*gint_dipoy)/(gamma*M*beta*c/q)*d_sbfact;
+    kappa(i) = (I(i)*g0_dipoy)/ridg*d_sbfact;
 elseif( nargin==2 && ele(i)=='P' )
-    kappa(i) = (I(i)*gint_pdy)/(gamma*M*beta*c/q)*pd_sbfact;
+    kappa(i) = (I(i)*g0_pdy)/ridg*pd_sbfact;
 else
    kappa(i) = 0;
    
--- Kappa2Current.m
+++ Kappa2Current.m
@@ -12,6 +12,7 @@ E = 10.0e3;
 T = q*E;
 gamma = 1+T/(M*c^2);
 beta = sqrt(1-gamma^-2);
+ridg = (gamma*M*beta*c/q); % beam rigidity
 
 % MODEL PARAMETERS -------------------------------------------------------
 % -- quad model
@@ -25,8 +26,9 @@ B_off = 1.35; % offset [Gauss]
 B_sbfact = sqrt(0.6945); % SB HE factor
 
 % -- dipole model
-gint_dipox = 0.146e-4; % int. quad grad By in dipo (due to sext.component) [T/A]
-gint_dipoy = 0.565e-4; % int. quad grad Bx in dipo (due to edge-focusing component) [T/A]
+leff_dipo = 3.819e-2; % HE dipole length [m]
+g0_dipox = 0.146e-4/leff_dipo; % quad grad By in dipo (due to sext.component) [T/A]
+g0_dipoy = 0.565e-4/leff_dipo; % quad grad Bx in dipo (due to edge-focusing component) [T/A]
 d_sbfact  = 1; % Has not yet been calculated for dipoles.
 
 % -- YQ/QR1 model
@@ -34,8 +36,9 @@ g0_panofsky = 1.01e-2; % quad peak grad per amp [T/m/A]
 panofsky_sbfact = 0.7224; 
 
 % -- PD model
-gint_pdx = 0.; % int. quad grad in PD (due to sext.component) has not been calc.
-gint_pdy = 0.041e-4; % int. quad grad in PD (due to sext.component) [T/A]
+leff_pd  = 5.006e-2; %HE PD length [m] 
+g0_pdx = 0./leff_pd; % HE quad grad in PD (due to sext.component) has not been calc.
+g0_pdy = 0.041e-4/leff_pd; % HE quad grad in PD (due to sext.component) [T/A]
 pd_sbfact  = 1; % Has not yet been calculated for dipoles.
 % ------------------------------------------------------------------------
 
@@ -43,23 +46,23 @@ pd_sbfact  = 1; % Has not yet been calculated for dipoles.
 for i=1:N
   
     if( nargin==1 || (nargin==2 && ele(i)=='Q') )
-        G = (gamma*M*beta*c/q)*kappa(i);
+        G = ridg*kappa(i);
         I(i) = G/g0/q_sbfact;
         %(gamma*M*beta*c/q)/g0; % not sure what this is here for...
     elseif( nargin==2 && ele(i)=='S' )
-        B = 2*(gamma*M*beta*c/q)*sqrt(kappa(i))/B_sbfact; %Tesla
+        B = 2*ridg*sqrt(kappa(i))/B_sbfact; %Tesla
         B = B*1e4; %Gauss
         B = B-B_off;
         I(i) = B/B0;
     elseif( nargin==2 && ele(i)=='Y' )
-        G = (gamma*M*beta*c/q)*kappa(i);
+        G = ridg*kappa(i);
         I(i) = G/g0_panofsky/panofsky_sbfact;
     elseif( nargin==2 && ele(i)=='D' )
-        G = (gamma*M*beta*c/q)*kappa(i);
-        I(i) = G/gint_dipoy/d_sbfact;
+        G = ridg*kappa(i);
+        I(i) = G/g0_dipoy/d_sbfact;
     elseif( nargin==2 && ele(i)=='P' )
-        G = (gamma*M*beta*c/q)*kappa(i);
-        I(i) = G/gint_pdy/pd_sbfact;
+        G = ridg*kappa(i);
+        I(i) = G/g0_pdy/pd_sbfact;
     end
     
 end
