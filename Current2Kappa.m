86c96430a4bfd29b44c44a57364e6033218cb42e
kruisard <kierstenjean@gmail.com>
2018-04-06 19:45:38 +0100

    Updated kappa/current conversion based on 4/6/18 meeting

--- Current2Kappa.m
+++ Current2Kappa.m
@@ -1,4 +1,8 @@
 function kappa = Current2Kappa( I, ele )
+N = length(I);
+kappa = zeros(1,N);
+
+% -- some constants
 q = 1.602e-19;
 M = 9.109e-31;
 c = 2.9979e8;
@@ -8,18 +12,31 @@ T = q*E;
 gamma = 1+T/(M*c^2);
 beta = sqrt(1-gamma^-2);
 
-% -- quad model parameters
-g0 = 3.608e-2; % quad peak grad per amp
-%q_sbfact = 0.8354; % SB HE factor
-q_sbfact = 0.7208; % SB HE factor updated (2006)
+% MODEL PARAMETERS -------------------------------------------------------
+% -- quad model
+g0 = 3.608e-2; % quad peak grad per amp [T/m/A]
+%q_sbfact = 0.8354; % RK HE factor, to be used w/ eff length 4.475 cm
+q_sbfact = 0.7208; % SB HE factor (2006 note), to be used w/ eff length 5.164 cm
 
-% -- solenoid model parameters
-B0 = 17.597; % peak field per amp
-B_off = 1.35; % offset
+% -- solenoid model
+B0 = 17.597; % peak field per amp [Gauss]
+B_off = 1.35; % offset [Gauss]
 B_sbfact = sqrt(0.6945); % SB HE factor
 
-N = length(I);
-kappa = zeros(1,N);
+% -- dipole model
+gint_dipox = 0.146e-4; % int. quad grad By in dipo (due to sext.component) [T/A]
+gint_dipoy = 0.565e-4; % int. quad grad Bx in dipo (due to edge-focusing component) [T/A]
+d_sbfact  = 1; % Has not yet been calculated for dipoles.
+
+% -- YQ/QR1 model
+g0_panofsky = 1.01e-2; % quad peak grad per amp [T/m/A]
+panofsky_sbfact = 0.7224; 
+
+% -- PD model
+gint_pdx = 0.; % int. quad grad in PD (due to sext.component) has not been calc.
+gint_pdy = 0.041e-4; % int. quad grad in PD (due to sext.component) [T/A]
+pd_sbfact  = 1; % Has not yet been calculated for dipoles.
+% ------------------------------------------------------------------------
 
 for i=1:N
 
@@ -27,7 +44,12 @@ if( nargin==1 || (nargin==2 && ele(i)=='Q') )
    kappa(i) = (I(i)*g0)/(gamma*M*beta*c/q)*q_sbfact;
 elseif( nargin==2 && ele(i)=='S' )
 	kappa(i) = ((I(i)*B0+B_off)*B_sbfact/(1e4*2*(gamma*M*beta*c/q)))^2;
-
+elseif( nargin==2 && ele(i)=='Y' )
+    kappa(i) = (I(i)*g0_panofsky)/(gamma*M*beta*c/q)*panofsky_sbfact;
+elseif( nargin==2 && ele(i)=='D' )
+    kappa(i) = (I(i)*gint_dipoy)/(gamma*M*beta*c/q)*d_sbfact;
+elseif( nargin==2 && ele(i)=='P' )
+    kappa(i) = (I(i)*gint_pdy)/(gamma*M*beta*c/q)*pd_sbfact;
 else
    kappa(i) = 0;
    
--- Kappa2Current.m
+++ Kappa2Current.m
@@ -1,4 +1,9 @@
 function I = Kappa2Current( kappa, ele )
+
+N = length(kappa);
+I = zeros(1,N);
+
+% -- some constants
 q = 1.602e-19;
 M = 9.109e-31;
 c = 2.9979e8;
@@ -8,18 +13,31 @@ T = q*E;
 gamma = 1+T/(M*c^2);
 beta = sqrt(1-gamma^-2);
 
-N = length(kappa);
-I = zeros(1,N);
-
+% MODEL PARAMETERS -------------------------------------------------------
 % -- quad model
-g0 = 3.608e-2; % quad peak grad per amp
-%q_sbfact = 0.8354; % SB HE factor
-q_sbfact = 0.7208; % SB HE factor updated (2006)
+g0 = 3.608e-2; % quad peak grad per amp [T/m/A]
+%q_sbfact = 0.8354; % RK HE factor, to be used w/ eff length 4.475 cm
+q_sbfact = 0.7208; % SB HE factor (2006 note), to be used w/ eff length 5.164 cm
 
 % -- solenoid model
-B0 = 17.597; % peak field per amp
-B_off = 1.35; % offset
+B0 = 17.597; % peak field per amp [Gauss]
+B_off = 1.35; % offset [Gauss]
 B_sbfact = sqrt(0.6945); % SB HE factor
+
+% -- dipole model
+gint_dipox = 0.146e-4; % int. quad grad By in dipo (due to sext.component) [T/A]
+gint_dipoy = 0.565e-4; % int. quad grad Bx in dipo (due to edge-focusing component) [T/A]
+d_sbfact  = 1; % Has not yet been calculated for dipoles.
+
+% -- YQ/QR1 model
+g0_panofsky = 1.01e-2; % quad peak grad per amp [T/m/A]
+panofsky_sbfact = 0.7224; 
+
+% -- PD model
+gint_pdx = 0.; % int. quad grad in PD (due to sext.component) has not been calc.
+gint_pdy = 0.041e-4; % int. quad grad in PD (due to sext.component) [T/A]
+pd_sbfact  = 1; % Has not yet been calculated for dipoles.
+% ------------------------------------------------------------------------
 
 % -- loop over all inputs
 for i=1:N
@@ -33,6 +51,15 @@ for i=1:N
         B = B*1e4; %Gauss
         B = B-B_off;
         I(i) = B/B0;
+    elseif( nargin==2 && ele(i)=='Y' )
+        G = (gamma*M*beta*c/q)*kappa(i);
+        I(i) = G/g0_panofsky/panofsky_sbfact;
+    elseif( nargin==2 && ele(i)=='D' )
+        G = (gamma*M*beta*c/q)*kappa(i);
+        I(i) = G/gint_dipoy/d_sbfact;
+    elseif( nargin==2 && ele(i)=='P' )
+        G = (gamma*M*beta*c/q)*kappa(i);
+        I(i) = G/gint_pdy/pd_sbfact;
     end
     
 end
